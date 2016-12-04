9764babae19a72f434cc18975baf8494ad97d871
Logan Dudley <arbybear@gmail.com>
2016-12-04 15:09:03 -0500

    cleaned up html and php, no user visible changes

--- AdvisingSite-master/src/student/homePage.php
+++ AdvisingSite-master/src/student/homePage.php
@@ -8,42 +8,36 @@ if($_SESSION["HAS_LOGGED_IN"] == false){
 ?>
 
 <html>
-<head>
-<link rel="stylesheet" type="text/css" href="../Styles/style.css">
-     <title>Student Homepage</title>
-</head>
-<body>
-<div id="content-container">
-<div id="content">
-      <h1>Student Home</h1>
+  <head>
+  <link rel="stylesheet" type="text/css" href="../Styles/style.css">
+  <title>Student Homepage</title>
+  </head>
+  
+  <body>
+    <div id="content-container">
+      <div id="content">
+        <h1>Student Home</h1>
 
-<?php if ($_SESSION["HAS_LOGGED_IN"]) { ?>
+        <h3>
+	Welcome <?php echo htmlspecialchars($_SESSION["STUDENT_EMAIL"]); ?>
+        </h3>
 
-    <h3>
-     Welcome <?php echo htmlspecialchars($_SESSION["STUDENT_EMAIL"]); ?>
-    </h3>
-<?php } ?>
+        <br>
+        <a href="viewMeeting.php">View Scheduled Appointment</a>
+        <br>
 
+        <br>
+        <a href="chooseMeeting.php">Schedule Advising Appointment</a>
+        <br>
 
+        <br>
+        <a href="cancelMeeting.php">Cancel Advising Appointment</a>
+        <br>
 
-<html>
-
-<br>
-<a href="viewMeeting.php">View Scheduled Appointment</a>
-<br>
-
-<br>
-<a href="chooseMeeting.php">Schedule Advising Appointment</a>
-<br>
-
-<br>
-<a href="cancelMeeting.php">Cancel Advising Appointment</a>
-<br>
-
-<br>
-<a href="logout.php">Log out</a>
-<br>
-</div>
-</div>
-</body>
+        <br>
+        <a href="logout.php">Log out</a>
+        <br>
+      </div>
+    </div>
+  </body>
 </html>
