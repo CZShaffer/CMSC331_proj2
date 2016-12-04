5ca49220f88e8e9b03fb41fb791e96fa751f9eb9
CZShaffer <pb10459@umbc.edu>
2016-12-04 16:57:17 -0500

    Deleted Calendar, fixed CSS (Advisor Homepage)

--- AdvisingSite-master/src/advisor/views/homepage.php
+++ AdvisingSite-master/src/advisor/views/homepage.php
@@ -113,121 +113,6 @@ function findStudentsInMeeting($meetingID)
     </a>
 
     <hr>
-    
-
-
-<?php
-
-function build_calendar($month,$year) {
-
-     // Create array containing abbreviations of days of week.
-     $daysOfWeek = array('S','M','T','W','T','F','S');
-
-     // What is the first day of the month in question?
-     $firstDayOfMonth = mktime(0,0,0,$month,1,$year);
-
-     // How many days does this month contain?
-     $numberDays = date('t',$firstDayOfMonth);
-
-     // Retrieve some information about the first day of the
-     // month in question.
-     $dateComponents = getdate($firstDayOfMonth);
-
-     // What is the name of the month in question?
-     $monthName = $dateComponents['month'];
-
-     // What is the index value (0-6) of the first day of the
-     // month in question.
-     $dayOfWeek = $dateComponents['wday'];
-
-     // Create the table tag opener and day headers
-
-     $calendar = "<table id='calendar'>";
-     $calendar .= "<div class='month'>$monthName $year</div>";
-     $calendar .= "<tr>";
-
-     // Create the calendar headers
-
-     foreach($daysOfWeek as $day) {
-          $calendar .= "<th class='header'>$day</th>";
-     } 
-
-     // Create the rest of the calendar
-
-     // Initiate the day counter, starting with the 1st.
-
-     $currentDay = 1;
-
-     $calendar .= "</tr><tr>";
-
-     // The variable $dayOfWeek is used to
-     // ensure that the calendar
-     // display consists of exactly 7 columns.
-
-     if ($dayOfWeek > 0) { 
-          $calendar .= "<td colspan='$dayOfWeek'>&nbsp;</td>"; 
-     }
-     
-     $month = str_pad($month, 2, "0", STR_PAD_LEFT);
-  
-     while ($currentDay <= $numberDays) {
-
-          // Seventh column (Saturday) reached. Start a new row.
-
-          if ($dayOfWeek == 7) {
-
-               $dayOfWeek = 0;
-               $calendar .= "</tr><tr>";
-
-          }
-          
-          $currentDayRel = str_pad($currentDay, 2, "0", STR_PAD_LEFT);
-          
-          $date = "$year-$month-$currentDayRel";
-
-          $calendar .= "<td class='day' rel='$date'>$currentDay</td>";
-
-          // Increment counters
- 
-          $currentDay++;
-          $dayOfWeek++;
-
-     }
-     
-     
-
-     // Complete the row of the last week in month, if necessary
-
-     if ($dayOfWeek != 7) { 
-     
-          $remainingDays = 7 - $dayOfWeek;
-          $calendar .= "<td colspan='$remainingDays'>&nbsp;</td>"; 
-
-     }
-     
-     $calendar .= "</tr>";
-
-     $calendar .= "</table>";
-
-     return $calendar;
-
-}
-
-
-     $dateComponents = getdate();
-
-     $month = $dateComponents['mon']; 			     
-     $year = $dateComponents['year'];
-
-     echo build_calendar($month,$year);
-
-?>
-
-
-
-
-
-
 
 
     <?php foreach ($allRows as $aRow) { ?>
@@ -314,7 +199,7 @@ function build_calendar($month,$year) {
         
                 <label>
                     Meeting Start Date </label>
-                    <input type="datetime-local" name="meetingStartTime">
+                    <input type="datetime-local" name="meetingStartTime"><br>
                
                 <?php
                 if (isset($_SESSION["ERROR_ADVISOR_MEETING_DATE_OR_TIME"])) {
@@ -325,7 +210,7 @@ function build_calendar($month,$year) {
             
                 <label>
                     Building Name</label>
-                    <input type="text" name="buildingName">
+                    <input type="text" name="buildingName"><br>
                 
                 <?php
                 if (isset($_SESSION["ERROR_ADVISOR_MEETING_BUILDING"])) {
@@ -336,7 +221,7 @@ function build_calendar($month,$year) {
             
                 <label>
                     Room Number</label>
-                    <input type="text" name="roomNumber">
+                    <input type="text" name="roomNumber"><br>
                 
                 <?php
                 if (isset($_SESSION["ERROR_ADVISOR_MEETING_ROOM"])) {
@@ -350,7 +235,7 @@ function build_calendar($month,$year) {
                     <select name="meetingType">
                         <option value="individual">Individual</option>
                         <option value="group">Group</option>
-                    </select>
+                    </select><br><br><br>
                 
             
             
