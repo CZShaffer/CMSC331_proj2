4376d506927387846c94b2b9c3789f6024baa769
CZShaffer <pb10459@umbc.edu>
2016-12-11 14:46:01 -0500

    Revert "Deleted Calendar, fixed CSS (Advisor Homepage)"
    This reverts commit 5ca49220f88e8e9b03fb41fb791e96fa751f9eb9.
    
    # Conflicts:
    #	AdvisingSite-master/src/advisor/views/homepage.php

--- AdvisingSite-master/src/advisor/views/homepage.php
+++ AdvisingSite-master/src/advisor/views/homepage.php
@@ -126,6 +126,124 @@ function findStudentsInMeeting($meetingID)
 
     <hr>
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+    
+
+
+<?php
+
+function build_calendar($month,$year) {
+
+     // Create array containing abbreviations of days of week.
+     $daysOfWeek = array('S','M','T','W','T','F','S');
+
+     // What is the first day of the month in question?
+     $firstDayOfMonth = mktime(0,0,0,$month,1,$year);
+
+     // How many days does this month contain?
+     $numberDays = date('t',$firstDayOfMonth);
+
+     // Retrieve some information about the first day of the
+     // month in question.
+     $dateComponents = getdate($firstDayOfMonth);
+
+     // What is the name of the month in question?
+     $monthName = $dateComponents['month'];
+
+     // What is the index value (0-6) of the first day of the
+     // month in question.
+     $dayOfWeek = $dateComponents['wday'];
+
+     // Create the table tag opener and day headers
+
+     $calendar = "<table id='calendar'>";
+     $calendar .= "<div class='month'>$monthName $year</div>";
+     $calendar .= "<tr>";
+
+     // Create the calendar headers
+
+     foreach($daysOfWeek as $day) {
+          $calendar .= "<th class='header'>$day</th>";
+     } 
+
+     // Create the rest of the calendar
+
+     // Initiate the day counter, starting with the 1st.
+
+     $currentDay = 1;
+
+     $calendar .= "</tr><tr>";
+
+     // The variable $dayOfWeek is used to
+     // ensure that the calendar
+     // display consists of exactly 7 columns.
+
+     if ($dayOfWeek > 0) { 
+          $calendar .= "<td colspan='$dayOfWeek'>&nbsp;</td>"; 
+     }
+     
+     $month = str_pad($month, 2, "0", STR_PAD_LEFT);
+  
+     while ($currentDay <= $numberDays) {
+
+          // Seventh column (Saturday) reached. Start a new row.
+
+          if ($dayOfWeek == 7) {
+
+               $dayOfWeek = 0;
+               $calendar .= "</tr><tr>";
+
+          }
+          
+          $currentDayRel = str_pad($currentDay, 2, "0", STR_PAD_LEFT);
+          
+          $date = "$year-$month-$currentDayRel";
+
+          $calendar .= "<td class='day' rel='$date'>$currentDay</td>";
+
+          // Increment counters
+ 
+          $currentDay++;
+          $dayOfWeek++;
+
+     }
+     
+     
+
+     // Complete the row of the last week in month, if necessary
+
+     if ($dayOfWeek != 7) { 
+     
+          $remainingDays = 7 - $dayOfWeek;
+          $calendar .= "<td colspan='$remainingDays'>&nbsp;</td>"; 
+
+     }
+     
+     $calendar .= "</tr>";
+
+     $calendar .= "</table>";
+
+     return $calendar;
+
+}
+
+
+     $dateComponents = getdate();
+
+     $month = $dateComponents['mon']; 			     
+     $year = $dateComponents['year'];
+
+     echo build_calendar($month,$year);
+
+?>
+
+
+
+
+
+
+>>>>>>> parent of 5ca4922... Deleted Calendar, fixed CSS (Advisor Homepage)
 
 =======
 >>>>>>> parent of 3d3d64e... Calendar so far (not much)
@@ -216,7 +334,7 @@ function findStudentsInMeeting($meetingID)
                 <label>
 <<<<<<< HEAD
                     Meeting Start Date </label>
-                    <input type="datetime-local" name="meetingStartTime"><br>
+                    <input type="datetime-local" name="meetingStartTime">
                
 =======
                     Meeting Start Date
@@ -235,7 +353,7 @@ function findStudentsInMeeting($meetingID)
                 <label>
 <<<<<<< HEAD
                     Building Name</label>
-                    <input type="text" name="buildingName"><br>
+                    <input type="text" name="buildingName">
                 
 =======
                     Building Name
@@ -254,7 +372,7 @@ function findStudentsInMeeting($meetingID)
                 <label>
 <<<<<<< HEAD
                     Room Number</label>
-                    <input type="text" name="roomNumber"><br>
+                    <input type="text" name="roomNumber">
                 
 =======
                     Room Number
@@ -281,6 +399,7 @@ function findStudentsInMeeting($meetingID)
                         <option value="individual">Individual</option>
                         <option value="group">Group</option>
 <<<<<<< HEAD
+<<<<<<< HEAD
                     </select><br><br><br>
 					<body onload="hidefield()">
 <div id="div1">
@@ -289,6 +408,11 @@ function findStudentsInMeeting($meetingID)
 		      <input type = "text" name = "maxStudents">  
 					<br><br><br>
             </div>
+=======
+                    </select>
+                
+            
+>>>>>>> parent of 5ca4922... Deleted Calendar, fixed CSS (Advisor Homepage)
             
 =======
                     </select>
