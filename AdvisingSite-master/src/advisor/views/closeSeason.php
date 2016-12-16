d63e04e0adcf75833dc095bcd5999906582ca94e
Logan Dudley <arbybear@gmail.com>
2016-12-16 15:13:43 -0500

    finish seasonOver

---
+++ AdvisingSite-master/src/advisor/views/closeSeason.php
@@ -0,0 +1,36 @@
+<?php
+  //include '../utils/CommonMethods.php';
+include '../utils/dbconfig.php';
+session_start();
+
+// redirect user to index.php if they haven't logged in
+if($_SESSION["HAS_LOGGED_IN"] == false){
+  header("Location: login.php");
+}
+
+$open_connection = connectToDB();
+
+// remove current entry in AdvisingSeason table
+$sql = "REMOVE * FROM AdvisingSeason";
+$results = $open_connection->query($sql);
+
+// add new entry setting isSeasonOver to false
+$sql = "INSERT INTO AdvisingSeason (`isSeasonOver`) VALUES ('true')";
+$results = $open_connection->query($sql);
+
+//$debug = true;
+//$COMMON = new Common($debug);
+//$filename = "openSeason.php";
+
+// remove current entry in AdvisingSeason table
+//$sql = "REMOVE * FROM AdvisingSeason";
+//$select_results = $COMMON->executequery($sql, $filename);
+
+// add new entry setting isSeasonOver to false
+//$sql = "INSERT INTO AdvisingSeason (`isSeasonOver`) VALUES ('false')";
+//$result = $COMMON->executeQuery($sql, $filename);
+
+$_SESSION["isSeasonOver"] = true;
+
+header("Location: homepage.php");
+?>
---
+++ AdvisingSite-master/src/advisor/views/closeSeasonConfirmation.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+
+// redirect user to index.php if they haven't logged in
+if(!isset($_SESSION["HAS_LOGGED_IN"])){
+  header("Location: login.php");
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Advising Scheduling</title>
+
+    <link rel="icon" type="image/x-icon" href="favicon.png"/>
+    <link rel="stylesheet" type="text/css" href="_________________________.css"/>
+  </head>
+
+  <body>
+
+    <h2>Close advising season</h2>
+    <p>Are you sure you want to close the scheduling site? This can be reversed.</p>
+
+  <a href="closeSeason.php">
+    <button type="button">Close season</button>
+  </a>
+
+  <br><br>
+  <a href="homepage.php">
+    <button type="button">Take me back</button>
+  </a>
+
+    </form>
+  </body>
+</html>
