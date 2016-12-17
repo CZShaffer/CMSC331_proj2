415e5faed9b359c3ca0adcc0efa6c1d72009b335
Phillip Hilliard <phi3@umbc.edu>
2016-12-17 16:24:48 -0500

    added alert to tell student if their meeting has been canceled

--- AdvisingSite-master/src/student/chooseMeeting.php
+++ AdvisingSite-master/src/student/chooseMeeting.php
@@ -13,7 +13,47 @@ if($_SESSION["HAS_LOGGED_IN"] == false){
 <link rel="icon" type="image/png" href="../Styles/images/umbc.png">
     <title>Choose Your Meeting</title>
 </head>
+<style>
+    div.meetingAlert {
+        padding: 20px;
+        background-color: #e53900;
+        color: white;
+    }
+
+    span.close {
+        margin-left: 15px;
+        color: white;
+        font-weight: bold;
+        float: right;
+        cursor: pointer;
+        transition: 0.35s;
+    }
+
+    .closebtn:hover {
+        color: black;
+    }
+</style>
 <body>
+<?php
+$filename = "chooseMeeting.php";
+$sqlCommand = "SELECT * FROM Student WHERE StudentID=".$_SESSION['STUDENT_ID'].";";
+$rs = $COMMON->executequery($sqlCommand,$filename);
+$row = mysql_fetch_row($rs);
+//echo("".$row[7]);
+if($row[7]=="meeting_deleted"){
+    echo('
+        <div class="meetingAlert">
+            <span class="close" onclick="this.parentElement.style.display=\'none\'">
+                &times;
+            </span>
+            <b>Warning!</b>  Your previous meeting has been deleted. Please schedule a new one.
+        </div>
+    ');
+    $sqlCommand = "UPDATE Student SET meetingStatus='meeting_not_made' WHERE StudentID=".$_SESSION['STUDENT_ID'].";";
+    $rs = $COMMON->executequery($sqlCommand,$filename);
+}
+
+?>
 <div id="content-container">
 <div id="content">
 <h1>Would you like a group meeting or an Individual Meeting</h1>
